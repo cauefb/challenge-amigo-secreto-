4b539263fb4fddc22b89eefda551a53240871270
cauefb <ferreira.caue07@gmail.com>
2025-03-17 20:42:03 -0300

    adiconar amigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const lista = document.querySelector('#listaAmigos')
+const resultado = document.querySelector('#resultado')
+const nome = document.querySelector('#amigo')
+const amigos = []
+
+function adicionarAmigo() {
+    if (nome.value.trim().length <= 0) {
+      alert('Digite um nome no campo!')
+      return
+    }
+    amigos.push(nome.value)
+    listarAmigos()
+    nome.value = ''
+  }
